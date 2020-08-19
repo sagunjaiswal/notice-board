0772ed98ec2cee7c491aeacbc3838fab7d2256cc
pronayguha <pronay.guha@aot.edu.in>
2020-08-19 22:15:47 +0530

    handling null input in ulpoad notice

--- frontEnd/src/helperMethod.js
+++ frontEnd/src/helperMethod.js
@@ -2,22 +2,26 @@ import axios from "axios";
 
 export const noticeUploadHandler = (e) => {
   const noticeFile = e.target.files[0];
-  const fileType = noticeFile.type;
-  const types = ["application/pdf", "image/jpeg"];
-  if (types.includes(fileType)) {
-    console.log("noticeUploadHandler -> noticeFile", noticeFile);
-    axios
-      .post()
-      .then((response) => {
-        console.log(response);
-        alert("YOUR NOTICE IS SUCCESSFULLY SUBMITTED");
-      })
-      .catch((error) => {
-        alert(
-          "DUE TO SOME TECHNICAL ERROR YOUR NOTICE CANNOT BE SUBMITTED AT THE MOMENT"
-        );
-      });
+  if (noticeFile) {
+    const fileType = noticeFile.type;
+    const types = ["application/pdf", "image/jpeg"];
+    if (types.includes(fileType)) {
+      console.log("noticeUploadHandler -> noticeFile", noticeFile);
+      // axios
+      //   .post()
+      //   .then((response) => {
+      //     console.log(response);
+      //     alert("YOUR NOTICE IS SUCCESSFULLY SUBMITTED");
+      //   })
+      //   .catch((error) => {
+      //     alert(
+      //       "DUE TO SOME TECHNICAL ERROR YOUR NOTICE CANNOT BE SUBMITTED AT THE MOMENT"
+      //     );
+      //   });
+    } else {
+      console.log("Invalid File");
+    }
   } else {
-    console.log("Invalid File");
+    console.log("Please Select a File");
   }
 };
