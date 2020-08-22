3f301638aa199baf28752525643e73bc7c2b5446
pronayguha <pronay.guha@aot.edu.in>
2020-08-23 01:37:05 +0530

    notice upload with  image file :frontEnd completed

--- frontEnd/src/helperMethod.js
+++ frontEnd/src/helperMethod.js
@@ -1,23 +1,31 @@
 import axios from "axios";
 
-export const noticeUploadHandler = (e) => {
-  const noticeFile = e.target.files[0];
-  if (noticeFile) {
-    const fileType = noticeFile.type;
+export const noticeUploadHandler = ({ title, desc, noticeImage }) => {
+  console.log("data:>>", title, desc, noticeImage);
+  if (noticeImage) {
+    const fileType = noticeImage.type;
     const types = ["application/pdf", "image/jpeg"];
     if (types.includes(fileType)) {
-      console.log("noticeUploadHandler -> noticeFile", noticeFile);
-      // axios
-      //   .post()
-      //   .then((response) => {
-      //     console.log(response);
-      //     alert("YOUR NOTICE IS SUCCESSFULLY SUBMITTED");
-      //   })
-      //   .catch((error) => {
-      //     alert(
-      //       "DUE TO SOME TECHNICAL ERROR YOUR NOTICE CANNOT BE SUBMITTED AT THE MOMENT"
-      //     );
-      //   });
+      console.log("noticeUploadHandler -> noticeImage", noticeImage);
+      const config = {
+        headers: { "content-type": "multipart/form-data" },
+      };
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("desc", desc);
+      formData.append("noticeImage", noticeImage);
+      axios
+        .post("http://localhost:5000/notice", formData, config)
+        .then((response) => {
+          console.log(response);
+          alert("YOUR NOTICE IS SUCCESSFULLY SUBMITTED");
+          // window.location.reload();
+        })
+        .catch((error) => {
+          alert(
+            "DUE TO SOME TECHNICAL ERROR YOUR NOTICE CANNOT BE SUBMITTED AT THE MOMENT"
+          );
+        });
     } else {
       console.log("Invalid File");
     }
