7308b670aab1e24df1ed7994d04c45888ebad062
SAGUN JAISWAL <63302439+sagunjaiswal@users.noreply.github.com>
2020-08-29 23:45:11 +0530

    png ang jpg rendering done

--- frontEnd/src/components/pages/DisplayNotices.js
+++ frontEnd/src/components/pages/DisplayNotices.js
@@ -18,16 +18,109 @@ const DisplayNotices = () => {
       });
   }, []);
 
+  const noticeRenderer = ({
+    noticeFileType,
+    noticeFile,
+    noticeDate,
+    title,
+    _id,
+  }) => {
+    if (noticeFileType === "application/pdf") {
+      // return uploadedNotices.length
+      // ? uploadedNotices.map((notice) => {
+      return (
+        <div className="notices-container">
+          <SingleNotice
+            key={_id}
+            notice={(noticeFileType, noticeFile, noticeDate, title, _id)}
+          />
+        </div>
+      );
+      // })
+      // : null;
+    }
+    // <a
+    //   key={_id}
+    //   href={`http://localhost:5000/${noticeFile}`}
+    //   rel="noopener noreferrer"
+    //   target="_blank"
+    // >
+    //   <div
+    //     style={{
+    //       marginTop: 20,
+    //       marginBottom: 20,
+    //       width: "200px",
+    //       display: "inline",
+    //     }}
+    //   >
+    //     <img src="/iconfinder_application-illustrator_8889.png" alt={title} />
+    //     <p>{title}</p>
+    //     <p>{noticeDate}</p>
+    //   </div>
+    // </a>
+    else {
+      return (
+        <a
+          key={_id}
+          href={`http://localhost:5000/${noticeFile}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <div
+            style={{
+              marginTop: 20,
+              marginBottom: 20,
+              width: "200px",
+              display: "inline",
+            }}
+          >
+            <img src="/icons8-noticeboard-96.png" alt="imageNotice" />
+            <p>{title}</p>
+          </div>
+        </a>
+      );
+    }
+  };
+
   const getContent = () => {
     return isLoading ? (
       <h1>Loading...</h1>
     ) : uploadedNotices.length ? (
+      // uploadedNotices.map((notice) => noticeRenderer(notice))
       uploadedNotices.map((notice) => {
-        return (
-          <div className="notices-container">
-            <SingleNotice key={notice._id} notice={notice} />
-          </div>
-        );
+        const { noticeFileType, noticeFile, noticeDate, title, _id } = notice;
+
+        if (noticeFileType === "application/pdf") {
+          // return uploadedNotices.length
+          // ? uploadedNotices.map((notice) => {
+          return (
+            <div className="notices-container">
+              <SingleNotice key={notice._id} notice={notice} />
+            </div>
+          );
+        } else {
+          return (
+            <div className="notices-container">
+              <a
+                key={_id}
+                href={`http://localhost:5000/${noticeFile}`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <div style={{ padding: "80px", paddingTop: "100px" }}>
+                  <img
+                    src={`http://localhost:5000/${noticeFile}`}
+                    alt="imageNotice"
+                    height="200px"
+                    width="200px"
+                  />
+                  <p style={{ color: "#6aa051" }}>{title}</p>
+                  <p>{noticeDate}</p>
+                </div>
+              </a>
+            </div>
+          );
+        }
       })
     ) : (
       <h1>No notice</h1>
