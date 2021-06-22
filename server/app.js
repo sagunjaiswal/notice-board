31554936aa68a65d0f7f1f7df75f143e9bdade6b
Pronay Guha <57461706+pronayguha13@users.noreply.github.com>
2021-06-23 00:32:31 +0530

    backend structure and script changed

--- server/app.js
+++ server/app.js
@@ -13,10 +13,6 @@ app.use(cors());
 // app.use("/uploads", express.static("uploads"));
 app.use(express.static(__dirname));
 
-//if this project runs online then it will check do we have an env online and if we use it locally its gonna use localhost:5000
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`The server has started on port : ${PORT}`));
-
 //set up mongoose
 mongoose.connect(
   process.env.MONGODB_CONNECTION_STRING,
@@ -51,3 +47,5 @@ app.use((req, res, next) => {
 //set up routes
 app.use("/users", require("./routes/UserRoute"));
 app.use("/notice", require("./routes/NoticeRoute"));
+
+module.exports = app;
